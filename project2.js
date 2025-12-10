2c05a99847b33ffa21ba39fec46c380b4c75e99d
willu628bruh <willu628@newschool.edu>
2025-12-10 15:26:17 -0500

    Update project2.js

--- project2.js
+++ project2.js
@@ -1,1 +1,20 @@
+let myColor;
 
+function setup() {
+  createCanvas(400, 400);
+  myColor = color(0, 255, 0);
+  noLoop(); // stop automatic looping
+}
+
+function draw() {
+  background(220);
+  fill(myColor);
+  noStroke();
+  ellipse(width/2, height/2, 150, 150);
+}
+
+// redraw only when mouse is pressed
+function mousePressed() {
+  myColor = color(random(255), random(255), random(255));
+  redraw(); // manually call draw() once
+}
