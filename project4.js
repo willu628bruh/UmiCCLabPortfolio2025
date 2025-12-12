06b8320fabe967d6045250403680e9f2ec8094ea
willu628bruh <willu628@newschool.edu>
2025-12-12 15:57:13 -0500

    Update project4.js

--- project4.js
+++ project4.js
@@ -1,6 +1,6 @@
 function setup() {
   // create the canvas
-  canvas = createCanvas(400, 400);
+  canvas = createCanvas(600, 900);
   // attach the canvas to the div in your HTML
   canvas.parent("sketch-container");
 }
